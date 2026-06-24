4f1c584d8cddaaa598b8ef43eba649c57feaf135
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Shared/PersistentCartShare/PersistentCartShareConfig.php
+++ src/Spryker/Shared/PersistentCartShare/PersistentCartShareConfig.php
@@ -12,11 +12,15 @@ use Spryker\Shared\Kernel\AbstractSharedConfig;
 class PersistentCartShareConfig extends AbstractSharedConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_TYPE_QUOTE = 'quote';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const SHARE_OPTION_KEY_PREVIEW = 'PREVIEW';
